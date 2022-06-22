9eaafdb96726587cd31bcb437b7642e362cdb088
JustAPC <andrea.pontillo@libero.it>
2022-06-22 16:09:30 +0200

    modifiche

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,41 +1,41 @@
 <template>
-  <div>
-    <header class="d-flex justify-content-center position-relative">
-      <NavbarItems />
-    </header>
+   <div>
+      <header class="d-flex justify-content-center position-relative text-center">
+         <NavbarItems />
+      </header>
 
-    <main class="hero-section d-flex">
-      <HeroDescription />
-      <div class="hero-img col-8"></div>
-    </main>
-  </div>
+      <main class="hero-section d-flex">
+         <HeroDescription />
+         <div class="hero-img col-8"></div>
+      </main>
+   </div>
 </template>
 
 <script>
-import NavbarItems from "./partials/NavbarItems.vue";
-import HeroDescription from "./partials/HeroDescription.vue";
+   import NavbarItems from "./partials/NavbarItems.vue";
+   import HeroDescription from "./partials/HeroDescription.vue";
 
-export default {
-  name: "HeaderComp",
-  components: {
-    NavbarItems,
-    HeroDescription,
-  },
-  props: {},
-};
+   export default {
+      name: "HeaderComp",
+      components: {
+         NavbarItems,
+         HeroDescription,
+      },
+      props: {},
+   };
 </script>
 
 <style scoped lang="scss">
-@import "../assets/styles/mystyles.scss";
-@import "../assets/styles/mystyle.css";
+   @import "../assets/styles/mystyles.scss";
+   @import "../assets/styles/mystyle.css";
 
-.hero-section {
-  height: 100vh;
-}
+   .hero-section {
+      height: 100vh;
+   }
 
-.hero-img {
-  background-image: url(../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg);
-  background-size: cover;
-  z-index: 0;
-}
+   .hero-img {
+      background-image: url(../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg);
+      background-size: cover;
+      z-index: 0;
+   }
 </style>
--- src/components/partials/NavbarItems.vue
+++ src/components/partials/NavbarItems.vue
@@ -1,101 +1,98 @@
 <template>
-  <div class="navbar-container col-10 position-absolute">
-    <div class="d-flex justify-content-between align-items-center">
-      <div>
-        <img src="../../assets/images/logotype.png" alt="" class="logo" />
-      </div>
+   <div class="navbar-container w-100 position-absolute">
+      <div class="d-flex justify-content-between align-items-center mx-5 py-4" height="50px">
+         <div>
+            <img src="../../assets/images/logotype.png" alt="" class="logo" />
+         </div>
 
-      <div>
-        <ul class="d-flex">
-          <li
-            v-for="(elm, i) in menuListItems"
-            :key="i"
-            class="text-uppercase pt-4 mx-3"
-            :class="{ active: elm.activePage }"
-          >
-            <a :href="elm.url">{{ elm.page }}</a>
-          </li>
-        </ul>
+         <div>
+            <ul class="d-flex align-items-center p-0 m-0">
+               <li v-for="(elm, i) in menuListItems" :key="i">
+                  <a :href="elm.url" class="text-uppercase py-4 mx-3" :class="{ active: elm.activePage }">
+                     {{ elm.page }}
+                  </a>
+               </li>
+            </ul>
+         </div>
       </div>
-    </div>
-  </div>
+   </div>
 </template>
 
 <script>
-export default {
-  name: "NavBar",
-  props: {},
+   export default {
+      name: "NavBar",
+      props: {},
 
-  data() {
-    return {
-      menuListItems: [
-        {
-          page: "Home",
-          url: "#",
-          activePage: true,
-        },
-        {
-          page: "About",
-          url: "#",
-          activePage: false,
-        },
-        {
-          page: "Projects",
-          url: "#",
-          activePage: false,
-        },
-        {
-          page: "Services",
-          url: "#",
-          activePage: false,
-        },
-        {
-          page: "Blog",
-          url: "#",
-          activePage: false,
-        },
-        {
-          page: "Contact",
-          url: "#",
-          activePage: false,
-        },
-      ],
-    };
-  },
-};
+      data() {
+         return {
+            menuListItems: [
+               {
+                  page: "Home",
+                  url: "#",
+                  activePage: true,
+               },
+               {
+                  page: "About",
+                  url: "#",
+                  activePage: false,
+               },
+               {
+                  page: "Projects",
+                  url: "#",
+                  activePage: false,
+               },
+               {
+                  page: "Services",
+                  url: "#",
+                  activePage: false,
+               },
+               {
+                  page: "Blog",
+                  url: "#",
+                  activePage: false,
+               },
+               {
+                  page: "Contact",
+                  url: "#",
+                  activePage: false,
+               },
+            ],
+         };
+      },
+   };
 </script>
 
 <style scoped lang="scss">
-@import "../../assets/styles/mystyles.scss";
-@import "../../assets/styles/mystyle.css";
+   @import "../../assets/styles/mystyles.scss";
+   @import "../../assets/styles/mystyle.css";
 
-.navbar-container {
-  z-index: 2;
-  top: 0;
-  left: 0;
-}
+   .navbar-container {
+      z-index: 2;
+      top: 0;
+      left: 0;
+   }
 
-.logo {
-  width: 36%;
-}
+   .logo {
+      width: 36%;
+   }
 
-.active {
-  border-top: 3px solid $border-color-white1;
-  a {
-    color: $text-color-white1;
-  }
-}
+   .active {
+      border-top: 3px solid $border-color-white1;
+      a {
+         color: $text-color-white1;
+      }
+   }
 
-ul li {
-  list-style-type: none;
-}
+   ul li {
+      list-style-type: none;
+   }
 
-li a {
-  color: $text-color-white2;
-  text-decoration: none;
-}
+   li a {
+      color: $text-color-white2;
+      text-decoration: none;
+   }
 
-a:hover {
-  color: $text-color-white1;
-}
+   a:hover {
+      color: $text-color-white1;
+   }
 </style>
